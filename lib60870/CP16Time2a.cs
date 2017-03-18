aa00da81a087b7d5fad3cd0ad8e9305b7df72c0c
zw <zw@HOME-SONY>
2017-03-18 10:37:12 +0800

    对几个时标进行解释和格式调整

--- lib60870/CP16Time2a.cs
+++ lib60870/CP16Time2a.cs
@@ -25,35 +25,41 @@ using System;
 
 namespace lib60870
 {
+    /// <summary>
+    /// 2字节时标，较少使用，表示范围是0-59999ms
+    /// </summary>
 	public class CP16Time2a
-	{
-		private byte[] encodedValue = new byte[2];
+    {
+        private byte[] encodedValue = new byte[2];
 
-		internal CP16Time2a (byte[] msg, int startIndex)
-		{
-			if (msg.Length < startIndex + 3)
-				throw new ASDUParsingException ("Message too small for parsing CP16Time2a");
+        internal CP16Time2a(byte[] msg, int startIndex)
+        {
+            if (msg.Length < startIndex + 3)
+                throw new ASDUParsingException("Message too small for parsing CP16Time2a");
 
-			for (int i = 0; i < 2; i++)
-				encodedValue [i] = msg [startIndex + i];
-		}
+            for (int i = 0; i < 2; i++)
+                encodedValue[i] = msg[startIndex + i];
+        }
 
-		public int ElapsedTimeInMs {
-			get {
-				return (encodedValue[0] + (encodedValue[1] * 0x100));
-			}
+        public int ElapsedTimeInMs
+        {
+            get
+            {
+                return (encodedValue[0] + (encodedValue[1] * 0x100));
+            }
 
-			set {
-				encodedValue [0] = (byte) (value % 0x100);
-				encodedValue [1] = (byte) (value / 0x100);
-			}
-		}
+            set
+            {
+                encodedValue[0] = (byte)(value % 0x100);
+                encodedValue[1] = (byte)(value / 0x100);
+            }
+        }
 
-		public byte[] GetEncodedValue() 
-		{
-			return encodedValue;
-		}
+        public byte[] GetEncodedValue()
+        {
+            return encodedValue;
+        }
 
-	}
+    }
 }
 
--- lib60870/CP24Time2a.cs
+++ lib60870/CP24Time2a.cs
@@ -25,83 +25,97 @@ using System;
 
 namespace lib60870
 {
-	public class CP24Time2a
-	{
-		private byte[] encodedValue = new byte[3];
+    /// <summary>
+    /// 3字节时标，较少使用，包含一个字节的分钟，2个字节的毫秒，毫秒需要mod1000获取实际毫秒
+    /// </summary>
+    public class CP24Time2a
+    {
+        private byte[] encodedValue = new byte[3];
 
-		internal CP24Time2a (byte[] msg, int startIndex)
-		{
-			if (msg.Length < startIndex + 3)
-				throw new ASDUParsingException ("Message too small for parsing CP24Time2a");
+        internal CP24Time2a(byte[] msg, int startIndex)
+        {
+            if (msg.Length < startIndex + 3)
+                throw new ASDUParsingException("Message too small for parsing CP24Time2a");
 
-			for (int i = 0; i < 3; i++)
-				encodedValue [i] = msg [startIndex + i];
-		}
+            for (int i = 0; i < 3; i++)
+                encodedValue[i] = msg[startIndex + i];
+        }
 
-		/// <summary>
-		/// Gets the totoal milliseconds of the elapsed time
-		/// </summary>
-		/// <returns>The milliseconds.</returns>
-		public int GetMilliseconds() {
+        /// <summary>
+        /// Gets the totoal milliseconds of the elapsed time
+        /// </summary>
+        /// <returns>The milliseconds.</returns>
+        public int GetMilliseconds()
+        {
 
-			int millies = Minute * (60000) + Second * 1000 + Millisecond;
+            int millies = Minute * (60000) + Second * 1000 + Millisecond;
 
-			return millies;
-		}
+            return millies;
+        }
 
-		/// <summary>
-		/// Gets the millisecond part of the time value
-		/// </summary>
-		/// <value>The millisecond.</value>
-		public int Millisecond {
-			get {
-				return (encodedValue[0] + (encodedValue[1] * 0x100)) % 1000;
-			}
-		}
+        /// <summary>
+        /// Gets the millisecond part of the time value
+        /// </summary>
+        /// <value>The millisecond.</value>
+        public int Millisecond
+        {
+            get
+            {
+                return (encodedValue[0] + (encodedValue[1] * 0x100)) % 1000;
+            }
+        }
 
-		public int Second {
-			get {
-				return  (encodedValue[0] + (encodedValue[1] * 0x100)) / 1000;
-			}
-		}
+        public int Second
+        {
+            get
+            {
+                return (encodedValue[0] + (encodedValue[1] * 0x100)) / 1000;
+            }
+        }
 
-		public int Minute {
-			get {
-				return (encodedValue [2] & 0x3f);
-			}
-		}
+        public int Minute
+        {
+            get
+            {
+                return (encodedValue[2] & 0x3f);
+            }
+        }
 
-		/// <summary>
-		/// Gets a value indicating whether this <see cref="lib60870.CP24Time2a"/> is invalid.
-		/// </summary>
-		/// <value><c>true</c> if invalid; otherwise, <c>false</c>.</value>
-		public bool Invalid {
-			get {
-				return ((encodedValue [2] & 0x80) == 0x80);
-			}
-		}
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="lib60870.CP24Time2a"/> is invalid.
+        /// </summary>
+        /// <value><c>true</c> if invalid; otherwise, <c>false</c>.</value>
+        public bool Invalid
+        {
+            get
+            {
+                return ((encodedValue[2] & 0x80) == 0x80);
+            }
+        }
 
-		/// <summary>
-		/// Gets a value indicating whether this <see cref="lib60870.CP24Time2a"/> was substitued by an intermediate station
-		/// </summary>
-		/// <value><c>true</c> if substitued; otherwise, <c>false</c>.</value>
-		public bool Substitued {
-			get {
-				return ((encodedValue [2] & 0x40) == 0x40);
-			}
-		}
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="lib60870.CP24Time2a"/> was substitued by an intermediate station
+        /// </summary>
+        /// <value><c>true</c> if substitued; otherwise, <c>false</c>.</value>
+        public bool Substitued
+        {
+            get
+            {
+                return ((encodedValue[2] & 0x40) == 0x40);
+            }
+        }
 
-		public byte[] GetEncodedValue() 
-		{
-			return encodedValue;
-		}
+        public byte[] GetEncodedValue()
+        {
+            return encodedValue;
+        }
 
 
-		public override string ToString ()
-		{
-			return string.Format ("[CP24Time2a: Millisecond={0}, Second={1}, Minute={2}, Invalid={3}, Substitued={4}]", Millisecond, Second, Minute, Invalid, Substitued);
-		}
-		
-	}
+        public override string ToString()
+        {
+            return string.Format("[CP24Time2a: Millisecond={0}, Second={1}, Minute={2}, Invalid={3}, Substitued={4}]", Millisecond, Second, Minute, Invalid, Substitued);
+        }
+
+    }
 }
 
