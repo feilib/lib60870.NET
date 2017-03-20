e33673d5ccac397b514b7218f34537ed14f60968
zw <zw@HOME-SONY>
2017-03-20 22:04:54 +0800

    对双点信息进行解析

--- lib60870/DoublePointInformation.cs
+++ lib60870/DoublePointInformation.cs
@@ -25,168 +25,258 @@ using System;
 
 namespace lib60870
 {
-	public enum DoublePointValue {
-		INTERMEDIATE = 0,
-		OFF = 1,
-		ON = 2,
-		INDETERMINATE = 3
-	}
+    /// <summary>
+    /// 双点遥信值
+    /// </summary>
+    public enum DoublePointValue
+    {
+        /// <summary>
+        /// 不确定，或者中间状态
+        /// </summary>
+        INTERMEDIATE = 0,
+        /// <summary>
+        /// 确定状态开
+        /// </summary>
+        OFF = 1,
+        /// <summary>
+        /// 确定状态合
+        /// </summary>
+        ON = 2,
+        /// <summary>
+        /// 不确定活中间状态
+        /// </summary>
+        INDETERMINATE = 3
+    }
 
+    /// <summary>
+    /// 双点遥信
+    /// </summary>
 	public class DoublePointInformation : InformationObject
-	{
-		override public TypeID Type {
-			get {
-				return TypeID.M_DP_NA_1;
-			}
-		}
+    {
+        override public TypeID Type
+        {
+            get
+            {
+                return TypeID.M_DP_NA_1;
+            }
+        }
 
-		override public bool SupportsSequence {
-			get {
-				return true;
-			}
-		}
+        /// <summary>
+        /// 支持连续
+        /// </summary>
+        override public bool SupportsSequence
+        {
+            get
+            {
+                return true;
+            }
+        }
 
-		private DoublePointValue value;
+        /// <summary>
+        /// 值
+        /// </summary>
+        private DoublePointValue value;
 
-		public DoublePointValue Value {
-			get {
-				return this.value;
-			}
-		}
+        public DoublePointValue Value
+        {
+            get
+            {
+                return this.value;
+            }
+        }
 
-		private QualityDescriptor quality;
+        /// <summary>
+        /// 品质描述词，低2位将被DPI（value）占据
+        /// </summary>
+        private QualityDescriptor quality;
 
-		public QualityDescriptor Quality {
-			get {
-				return this.quality;
-			}
-		}
+        public QualityDescriptor Quality
+        {
+            get
+            {
+                return this.quality;
+            }
+        }
 
-		public DoublePointInformation(int ioa, DoublePointValue value, QualityDescriptor quality)
-			: base(ioa)
-		{
-			this.value = value;
-			this.quality = quality;
-		}
+        /// <summary>
+        /// 使用地址、值、品质描述词创建双点信息
+        /// </summary>
+        /// <param name="ioa"></param>
+        /// <param name="value"></param>
+        /// <param name="quality"></param>
+        public DoublePointInformation(int ioa, DoublePointValue value, QualityDescriptor quality)
+            : base(ioa)
+        {
+            this.value = value;
+            this.quality = quality;
+        }
 
-		internal DoublePointInformation (ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
-			base(parameters, msg, startIndex, isSequence)
-		{
-			if (!isSequence)
-				startIndex += parameters.SizeOfIOA; /* skip IOA */
+        /// <summary>
+        /// 解析报文
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <param name="msg"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="isSequence"></param>
+        internal DoublePointInformation(ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
+            base(parameters, msg, startIndex, isSequence)
+        {
+            if (!isSequence)
+                startIndex += parameters.SizeOfIOA; /* skip IOA */
 
-			/* parse DIQ (double point information with qualitiy) */
-			byte diq = msg [startIndex++];
+            /* parse DIQ (double point information with qualitiy) */
+            byte diq = msg[startIndex++];
 
-			value = (DoublePointValue)(diq & 0x03);
+            value = (DoublePointValue)(diq & 0x03);
 
-			quality = new QualityDescriptor ((byte) (diq & 0xf0));
-		}
+            quality = new QualityDescriptor((byte)(diq & 0xf0));
+        }
 
-		internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence) {
-			base.Encode(frame, parameters, isSequence);
+        /// <summary>
+        /// 编码
+        /// </summary>
+        /// <param name="frame"></param>
+        /// <param name="parameters"></param>
+        /// <param name="isSequence"></param>
+        internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence)
+        {
+            base.Encode(frame, parameters, isSequence);
 
-			byte val = quality.EncodedValue;
+            byte val = quality.EncodedValue;
 
-			val += (byte)value;
+            val += (byte)value;
 
-			frame.SetNextByte (val);
-		}
-	}
+            frame.SetNextByte(val);
+        }
+    }
 
-	public class DoublePointWithCP24Time2a : DoublePointInformation
-	{
-		override public TypeID Type {
-			get {
-				return TypeID.M_DP_TA_1;
-			}
-		}
+    /// <summary>
+    /// 带短时标的双点信息
+    /// </summary>
+    public class DoublePointWithCP24Time2a : DoublePointInformation
+    {
+        override public TypeID Type
+        {
+            get
+            {
+                return TypeID.M_DP_TA_1;
+            }
+        }
 
-		override public bool SupportsSequence {
-			get {
-				return false;
-			}
-		}
+        override public bool SupportsSequence
+        {
+            get
+            {
+                return false;
+            }
+        }
 
-		private CP24Time2a timestamp;
+        private CP24Time2a timestamp;
 
-		public CP24Time2a Timestamp {
-			get {
-				return this.timestamp;
-			}
-		}
+        public CP24Time2a Timestamp
+        {
+            get
+            {
+                return this.timestamp;
+            }
+        }
 
-		public DoublePointWithCP24Time2a(int ioa, DoublePointValue value, QualityDescriptor quality, CP24Time2a timestamp)
-			: base(ioa, value, quality)
-		{
-			this.timestamp = timestamp;
-		}
+        public DoublePointWithCP24Time2a(int ioa, DoublePointValue value, QualityDescriptor quality, CP24Time2a timestamp)
+            : base(ioa, value, quality)
+        {
+            this.timestamp = timestamp;
+        }
 
-		internal DoublePointWithCP24Time2a (ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
-		base(parameters, msg, startIndex, isSequence)
-		{
-			if (!isSequence)
-				startIndex += parameters.SizeOfIOA; /* skip IOA */
+        internal DoublePointWithCP24Time2a(ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
+        base(parameters, msg, startIndex, isSequence)
+        {
+            if (!isSequence)
+                startIndex += parameters.SizeOfIOA; /* skip IOA */
 
-			startIndex += 1; /* skip DIQ */
+            startIndex += 1; /* skip DIQ */
 
-			/* parse CP24Time2a (time stamp) */
-			timestamp = new CP24Time2a (msg, startIndex);
-		}
+            /* parse CP24Time2a (time stamp) */
+            timestamp = new CP24Time2a(msg, startIndex);
+        }
 
-		internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence) {
-			base.Encode(frame, parameters, isSequence);
+        internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence)
+        {
+            base.Encode(frame, parameters, isSequence);
 
-			frame.AppendBytes (timestamp.GetEncodedValue ());
-		}
-	}
+            frame.AppendBytes(timestamp.GetEncodedValue());
+        }
+    }
 
-	public class DoublePointWithCP56Time2a : DoublePointInformation
-	{
-		override public TypeID Type {
-			get {
-				return TypeID.M_DP_TB_1;
-			}
-		}
+    /// <summary>
+    /// 带长时标的双点信息
+    /// </summary>
+    public class DoublePointWithCP56Time2a : DoublePointInformation
+    {
+        override public TypeID Type
+        {
+            get
+            {
+                return TypeID.M_DP_TB_1;
+            }
+        }
 
-		override public bool SupportsSequence {
-			get {
-				return false;
-			}
-		}
+        override public bool SupportsSequence
+        {
+            get
+            {
+                return false;
+            }
+        }
 
-		private CP56Time2a timestamp;
+        private CP56Time2a timestamp;
 
-		public CP56Time2a Timestamp {
-			get {
-				return this.timestamp;
-			}
-		}
+        public CP56Time2a Timestamp
+        {
+            get
+            {
+                return this.timestamp;
+            }
+        }
 
-		public DoublePointWithCP56Time2a(int ioa, DoublePointValue value, QualityDescriptor quality, CP56Time2a timestamp)
-			: base(ioa, value, quality)
-		{
-			this.timestamp = timestamp;
-		}
+        /// <summary>
+        /// 创建
+        /// </summary>
+        /// <param name="ioa"></param>
+        /// <param name="value"></param>
+        /// <param name="quality"></param>
+        /// <param name="timestamp"></param>
+        public DoublePointWithCP56Time2a(int ioa, DoublePointValue value, QualityDescriptor quality, CP56Time2a timestamp)
+            : base(ioa, value, quality)
+        {
+            this.timestamp = timestamp;
+        }
 
-		internal DoublePointWithCP56Time2a (ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
-			base(parameters, msg, startIndex, isSequence)
-		{
-			if (!isSequence)
-				startIndex += parameters.SizeOfIOA; /* skip IOA */
+        /// <summary>
+        /// 解析
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <param name="msg"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="isSequence"></param>
+        internal DoublePointWithCP56Time2a(ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
+            base(parameters, msg, startIndex, isSequence)
+        {
+            if (!isSequence)
+                startIndex += parameters.SizeOfIOA; /* skip IOA */
 
-			startIndex += 1; /* skip DIQ */
+            startIndex += 1; /* skip DIQ */
 
-			/* parse CP56Time2a (time stamp) */
-			timestamp = new CP56Time2a (msg, startIndex);
-		}
+            /* parse CP56Time2a (time stamp) */
+            timestamp = new CP56Time2a(msg, startIndex);
+        }
 
-		internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence) {
-			base.Encode(frame, parameters, isSequence);
+        internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence)
+        {
+            base.Encode(frame, parameters, isSequence);
 
-			frame.AppendBytes (timestamp.GetEncodedValue ());
-		}
-	}
+            frame.AppendBytes(timestamp.GetEncodedValue());
+        }
+    }
 
 }
 
