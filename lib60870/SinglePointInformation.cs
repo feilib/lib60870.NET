297365e9474c82f2e249e9c389f56e8a126faaa8
zw <zw@HOME-SONY>
2017-03-20 21:54:44 +0800

    对单点信息进行了注释

--- lib60870/SinglePointInformation.cs
+++ lib60870/SinglePointInformation.cs
@@ -23,232 +23,353 @@ using System;
 
 namespace lib60870
 {
+    /// <summary>
+    /// 单点信息
+    /// </summary>
 	public class SinglePointInformation : InformationObject
-	{
-		override public TypeID Type {
-			get {
-				return TypeID.M_SP_NA_1;
-			}
-		}
+    {
+        /// <summary>
+        /// TypeID.M_SP_NA_1;
+        /// </summary>
+        override public TypeID Type
+        {
+            get
+            {
+                return TypeID.M_SP_NA_1;
+            }
+        }
 
-		override public bool SupportsSequence {
-			get {
-				return true;
-			}
-		}
+        override public bool SupportsSequence
+        {
+            get
+            {
+                return true;
+            }
+        }
 
-		private bool value;
+        /// <summary>
+        /// 实际单点值，bit1
+        /// </summary>
+        private bool value;
 
-		public bool Value {
-			get {
-				return this.value;
-			}
-		}
+        /// <summary>
+        /// 实际点值
+        /// </summary>
+        public bool Value
+        {
+            get
+            {
+                return this.value;
+            }
+        }
 
-		private QualityDescriptor quality;
+        /// <summary>
+        /// 品质描述词
+        /// </summary>
+        private QualityDescriptor quality;
 
-		public QualityDescriptor Quality {
-			get {
-				return this.quality;
-			}
-		}
+        /// <summary>
+        /// 品质描述词
+        /// </summary>
+        public QualityDescriptor Quality
+        {
+            get
+            {
+                return this.quality;
+            }
+        }
 
-		internal SinglePointInformation (ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
-			base(parameters, msg, startIndex, isSequence)
-		{
-			if (!isSequence)
-				startIndex += parameters.SizeOfIOA; /* skip IOA */
+        /// <summary>
+        /// 通过信息内容解析报文
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <param name="msg"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="isSequence"></param>
+        internal SinglePointInformation(ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
+            base(parameters, msg, startIndex, isSequence)
+        {
+            if (!isSequence)
+                startIndex += parameters.SizeOfIOA; /* skip IOA 跳过信息体地址 */
 
-			/* parse SIQ (single point information with qualitiy) */
-			byte siq = msg [startIndex++];
+            /* parse SIQ (single point information with qualitiy)  解析实际值 */
+            byte siq = msg[startIndex++];
 
-			value = ((siq & 0x01) == 0x01);
+            //获得值 bit1
+            value = ((siq & 0x01) == 0x01);
 
-			quality = new QualityDescriptor ((byte) (siq & 0xf0));
-		}
+            //品质描述词，只要高4位，低位没用 bit5-8
+            quality = new QualityDescriptor((byte)(siq & 0xf0));
+        }
+
+        /// <summary>
+        /// 构造一个单点信息，使用信息地址、值、品质描述词
+        /// </summary>
+        /// <param name="objectAddress">信息体地址</param>
+        /// <param name="value">单点值</param>
+        /// <param name="quality">品质描述词（OV无用，直接被覆盖了）</param>
+        public SinglePointInformation(int objectAddress, bool value, QualityDescriptor quality) :
+            base(objectAddress)
+        {
+            this.value = value;
+            this.quality = quality;
+
+        }
+
+        /// <summary>
+        /// 编码进入frame
+        /// </summary>
+        /// <param name="frame"></param>
+        /// <param name="parameters"></param>
+        /// <param name="isSequence"></param>
+        internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence)
+        {
+            base.Encode(frame, parameters, isSequence);
+
+            byte val = quality.EncodedValue;
+
+            //设置值，如果有值为true，则ov反相  todo，这个算法需要验证。。。。或者需要直接覆盖
+            if (value)
+                val++;
+
+            frame.SetNextByte(val);
+        }
+
+    }
+
+    /// <summary>
+    /// 带时标的单点信息（用的少）
+    /// </summary>
+    public class SinglePointWithCP24Time2a : SinglePointInformation
+    {
+        override public TypeID Type
+        {
+            get
+            {
+                return TypeID.M_SP_TA_1;
+            }
+        }
+
+        /// <summary>
+        /// 带时标的就不支持连续了？
+        /// </summary>
+        override public bool SupportsSequence
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        private CP24Time2a timestamp;
+
+        public CP24Time2a Timestamp
+        {
+            get
+            {
+                return this.timestamp;
+            }
+        }
+
+        /// <summary>
+        /// 解析
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <param name="msg"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="isSequence"></param>
+        internal SinglePointWithCP24Time2a(ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
+        base(parameters, msg, startIndex, isSequence)
+        {
+            if (!isSequence)
+                startIndex += parameters.SizeOfIOA; /* skip IOA */
+
+            startIndex += 1; /* skip SIQ */
+
+            /* parse CP24Time2a (time stamp) */
+            timestamp = new CP24Time2a(msg, startIndex);
+        }
+
+        /// <summary>
+        /// 新建
+        /// </summary>
+        /// <param name="objectAddress"></param>
+        /// <param name="value"></param>
+        /// <param name="quality"></param>
+        /// <param name="timestamp"></param>
+        public SinglePointWithCP24Time2a(int objectAddress, bool value, QualityDescriptor quality, CP24Time2a timestamp) :
+            base(objectAddress, value, quality)
+        {
+            this.timestamp = timestamp;
+        }
+
+        internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence)
+        {
+            base.Encode(frame, parameters, isSequence);
+
+            frame.AppendBytes(timestamp.GetEncodedValue());
+        }
+    }
+
+    /// <summary>
+    /// Single point with CP56Time2a timestamp (M_SP_TB_1)
+    /// 带长时标的单点信息
+    /// </summary>
+    public class SinglePointWithCP56Time2a : SinglePointInformation
+    {
+        override public TypeID Type
+        {
+            get
+            {
+                return TypeID.M_SP_TB_1;
+            }
+        }
+
+        /// <summary>
+        /// 不支持连续
+        /// </summary>
+        override public bool SupportsSequence
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        private CP56Time2a timestamp;
+
+        public CP56Time2a Timestamp
+        {
+            get
+            {
+                return this.timestamp;
+            }
+        }
+
+        /// <summary>
+        /// 通过信息体解析
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <param name="msg"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="isSequence"></param>
+        internal SinglePointWithCP56Time2a(ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
+        base(parameters, msg, startIndex, isSequence)
+        {
+            if (!isSequence)
+                startIndex += parameters.SizeOfIOA; /* skip IOA */
+
+            startIndex += 1; /* skip SIQ */
+
+            /* parse CP56Time2a (time stamp) */
+            timestamp = new CP56Time2a(msg, startIndex);
+        }
+
+        /// <summary>
+        /// 创建一个
+        /// </summary>
+        /// <param name="objectAddress"></param>
+        /// <param name="value"></param>
+        /// <param name="quality"></param>
+        /// <param name="timestamp"></param>
+        public SinglePointWithCP56Time2a(int objectAddress, bool value, QualityDescriptor quality, CP56Time2a timestamp) :
+        base(objectAddress, value, quality)
+        {
+            this.timestamp = timestamp;
+        }
+
+        /// <summary>
+        /// 编码
+        /// </summary>
+        /// <param name="frame"></param>
+        /// <param name="parameters"></param>
+        /// <param name="isSequence"></param>
+        internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence)
+        {
+            base.Encode(frame, parameters, isSequence);
+
+            frame.AppendBytes(timestamp.GetEncodedValue());
+        }
+    }
+
+    /// <summary>
+    /// 带变位检出单点信息（用的少）
+    /// </summary>
+    public class PackedSinglePointWithSCD : InformationObject
+    {
+        override public TypeID Type
+        {
+            get
+            {
+                return TypeID.M_PS_NA_1;
+            }
+        }
+
+        override public bool SupportsSequence
+        {
+            get
+            {
+                return true;
+            }
+        }
 
 
-		public SinglePointInformation(int objectAddress, bool value, QualityDescriptor quality):
-			base(objectAddress)
-		{
-			this.value = value;
-			this.quality = quality;
-		}
+        private StatusAndStatusChangeDetection scd;
 
-		internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence) {
-			base.Encode(frame, parameters, isSequence);
+        private QualityDescriptor qds;
 
-			byte val = quality.EncodedValue;
+        public StatusAndStatusChangeDetection SCD
+        {
+            get
+            {
+                return this.scd;
+            }
+            set
+            {
+                scd = value;
+            }
+        }
 
-			if (value)
-				val++;
+        public QualityDescriptor QDS
+        {
+            get
+            {
+                return this.qds;
+            }
+            set
+            {
+                qds = value;
+            }
+        }
 
-			frame.SetNextByte (val);
-		}
+        internal PackedSinglePointWithSCD(ConnectionParameters parameters, byte[] msg, int startIndex, bool isSquence) :
+            base(parameters, msg, startIndex, isSquence)
+        {
+            if (!isSquence)
+                startIndex += parameters.SizeOfIOA; /* skip IOA */
 
-	}
+            scd = new StatusAndStatusChangeDetection(msg, startIndex);
+            startIndex += 4;
 
-	public class SinglePointWithCP24Time2a : SinglePointInformation
-	{
-		override public TypeID Type {
-			get {
-				return TypeID.M_SP_TA_1;
-			}
-		}
+            qds = new QualityDescriptor(msg[startIndex++]);
+        }
 
-		override public bool SupportsSequence {
-			get {
-				return false;
-			}
-		}
+        public PackedSinglePointWithSCD(int objectAddress, StatusAndStatusChangeDetection scd, QualityDescriptor quality)
+            : base(objectAddress)
+        {
+            this.scd = scd;
+            this.qds = quality;
+        }
 
-		private CP24Time2a timestamp;
+        internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence)
+        {
+            base.Encode(frame, parameters, isSequence);
 
-		public CP24Time2a Timestamp {
-			get {
-				return this.timestamp;
-			}
-		}
+            frame.AppendBytes(scd.GetEncodedValue());
 
-		internal SinglePointWithCP24Time2a (ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
-		base(parameters, msg, startIndex, isSequence)
-		{
-			if (!isSequence)
-				startIndex += parameters.SizeOfIOA; /* skip IOA */
-		
-			startIndex += 1; /* skip SIQ */
-
-			/* parse CP24Time2a (time stamp) */
-			timestamp = new CP24Time2a (msg, startIndex);
-		}
-
-		public SinglePointWithCP24Time2a(int objectAddress, bool value, QualityDescriptor quality, CP24Time2a timestamp):
-			base(objectAddress, value, quality)
-		{
-			this.timestamp = timestamp;
-		}
-
-		internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence) {
-			base.Encode(frame, parameters, isSequence);
-
-			frame.AppendBytes (timestamp.GetEncodedValue ());
-		}
-	}
-
-	/// <summary>
-	/// Single point with CP56Time2a timestamp (M_SP_TB_1)
-	/// </summary>
-	public class SinglePointWithCP56Time2a : SinglePointInformation
-	{
-		override public TypeID Type {
-			get {
-				return TypeID.M_SP_TB_1;
-			}
-		}
-
-		override public bool SupportsSequence {
-			get {
-				return false;
-			}
-		}
-
-		private CP56Time2a timestamp;
-
-		public CP56Time2a Timestamp {
-			get {
-				return this.timestamp;
-			}
-		}
-
-		internal SinglePointWithCP56Time2a (ConnectionParameters parameters, byte[] msg, int startIndex, bool isSequence) :
-		base(parameters, msg, startIndex, isSequence)
-		{
-			if (!isSequence)
-				startIndex += parameters.SizeOfIOA; /* skip IOA */
-
-			startIndex += 1; /* skip SIQ */
-
-			/* parse CP56Time2a (time stamp) */
-			timestamp = new CP56Time2a (msg, startIndex);
-		}
-
-		public SinglePointWithCP56Time2a(int objectAddress, bool value, QualityDescriptor quality, CP56Time2a timestamp):
-		base(objectAddress, value, quality)
-		{
-			this.timestamp = timestamp;
-		}
-
-		internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence) {
-			base.Encode(frame, parameters, isSequence);
-
-			frame.AppendBytes (timestamp.GetEncodedValue ());
-		}
-	}
-
-	public class PackedSinglePointWithSCD : InformationObject
-	{
-		override public TypeID Type {
-			get {
-				return TypeID.M_PS_NA_1;
-			}
-		}
-
-		override public bool SupportsSequence {
-			get {
-				return true;
-			}
-		}
-
-
-		private StatusAndStatusChangeDetection scd;
-
-		private QualityDescriptor qds;
-
-		public StatusAndStatusChangeDetection SCD {
-			get {
-				return this.scd;
-			}
-			set {
-				scd = value;
-			}
-		}
-
-		public QualityDescriptor QDS {
-			get {
-				return this.qds;
-			}
-			set {
-				qds = value;
-			}
-		}
-
-		internal PackedSinglePointWithSCD (ConnectionParameters parameters, byte[] msg, int startIndex, bool isSquence) :
-			base(parameters, msg, startIndex, isSquence)
-		{
-			if (!isSquence)
-				startIndex += parameters.SizeOfIOA; /* skip IOA */
-
-			scd = new StatusAndStatusChangeDetection (msg, startIndex);
-			startIndex += 4;
-
-			qds = new QualityDescriptor (msg [startIndex++]);
-		}
-			
-		public PackedSinglePointWithSCD(int objectAddress, StatusAndStatusChangeDetection scd, QualityDescriptor quality)
-			:base(objectAddress)
-		{
-			this.scd = scd;
-			this.qds = quality;
-		}
-
-		internal override void Encode(Frame frame, ConnectionParameters parameters, bool isSequence) {
-			base.Encode(frame, parameters, isSequence);
-
-			frame.AppendBytes (scd.GetEncodedValue ());
-
-			frame.SetNextByte (qds.EncodedValue);
-		}
-	}
+            frame.SetNextByte(qds.EncodedValue);
+        }
+    }
 
 }
 
