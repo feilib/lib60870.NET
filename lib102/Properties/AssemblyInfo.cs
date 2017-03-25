40adf81969b0ee4c38a1b949b3a1893051892af5
zw <zw@HOME-SONY>
2017-03-25 17:13:35 +0800

    删掉之前简历的“lib60870.102"，改名为"lib102"，便于库引用时编写

--- lib102/Properties/AssemblyInfo.cs
+++ lib102/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("lib60870.102")]
+[assembly: AssemblyTitle("lib102")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("lib60870.102")]
+[assembly: AssemblyProduct("lib102")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("14d8a128-94b8-4e16-930d-71aaa54789ff")]
+[assembly: Guid("18fa9263-0175-4fd5-9f9d-992d4b0e5186")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
